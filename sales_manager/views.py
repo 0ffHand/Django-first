273da95b6bf18481b86076e3167f560fea4b97d9
OffHand <ka.palcheuski@gmail.com>
2021-05-19 21:22:28 +0300

    commit 100%

--- sales_manager/views.py
+++ sales_manager/views.py
@@ -8,3 +8,7 @@ def main_page(request):
     return render(request, "sales_manager/index.html", context=context)
 
 
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    context = {"book": book}
+    return render(request, "sales_manager/book_detail.html", context=context)
