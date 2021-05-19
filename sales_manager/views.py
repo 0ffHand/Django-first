f39f8d5370304bed6ba775388f1261528572f1fa
OffHand <ka.palcheuski@gmail.com>
2021-05-19 20:59:49 +0300

    commit 100%

--- sales_manager/views.py
+++ sales_manager/views.py
@@ -4,7 +4,7 @@ from sales_manager.models import Book
 
 def main_page(request):
     query_set = Book.objects.all()
-    context = {"query_set": query_set}
+    context = {"books": query_set}
     return render(request, "sales_manager/index.html", context=context)
 
 
